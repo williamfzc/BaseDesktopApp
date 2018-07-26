2df307444109651c4ae90a320942bba88e84cc7a
William Feng <178894043@qq.com>
2018-07-26 14:36:18 +0800

    与命令行的交互

--- src/renderer/components/Dev.vue
+++ src/renderer/components/Dev.vue
@@ -1,5 +1,43 @@
 <template>
   <div>
-    <h2>hello i'm dev</h2>
+    <h2> 该页面用于确保electron与命令行的交互正常，输入指令点击运行即可看到结果。 </h2>
+    <p>{{cmdResult}}</p>
+    <el-container>
+      <el-input
+        placeholder="输入命令行指令"
+        v-model="cmdInput"
+        clearable>
+      </el-input>
+      <el-button plain @click="execCmd">运行</el-button>
+    </el-container>
   </div>
 </template>
+
+<script>
+const { exec } = require('child_process')
+export default {
+  data() {
+    return {
+      cmdResult: '',
+      cmdInput: ''
+    }
+  },
+  methods: {
+    execCmd: function() {
+      var loadingObject = this.$startLoading()
+      console.log('ready to exec: ' + this.cmdInput)
+      exec(this.cmdInput, (error, stdout, stderr) => {
+        if (error) {
+          console.log('get a error: ' + error)
+          this.cmdResult = error
+          return
+        }
+        console.log(stdout)
+        console.log(stderr)
+        this.cmdResult = stdout + '\n' + stderr
+        loadingObject.close()
+      })
+    }
+  }
+}
+</script>
--- src/renderer/components/Home.vue
+++ src/renderer/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <img src="../assets/logo.png">
+    <h2>点击后会出现加载动画与提示，1s后消失</h2>
     <div>
       <el-button @click="startHacking">Start</el-button>
     </div>
@@ -21,13 +21,13 @@ export default {
       var loadingObject = this.$startLoading()
       setTimeout(()=>{
         loadingObject.close()
-      }, 2000)
+      }, 1000)
 
       this.$notify({
         title: 'It works!',
         type: 'success',
         message: 'We\'ve laid the ground work for you. It\'s time for you to build something epic!',
-        duration: 5000
+        duration: 1000
       })
     }
   }
